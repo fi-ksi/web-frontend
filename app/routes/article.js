013de3376289b8ab5b07fbb01f54f6ce8a981b11
Jan Mrázek <email@honzamrazek.cz>
2015-08-06 22:50:48 +0200

    Scroll-to-top when changing route added

--- app/routes/article.js
+++ app/routes/article.js
@@ -1,6 +1,7 @@
 import Ember from "ember";
+import ResetScroll from '../mixins/reset-scroll';
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(ResetScroll, {
 	model: function(params) {
 		console.log(params);
 		return this.store.find("article", params["article_id"]);
