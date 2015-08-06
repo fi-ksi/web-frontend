09d583948a83a2bd9b2bf0cc729ec77fbf7fba9a
xlauko <410438@mail.muni.cz>
2015-08-06 23:52:10 +0200

    article-list.js preklep oprava

--- app/components/articles-list.js
+++ app/components/articles-list.js
@@ -3,7 +3,7 @@ import Ember from "ember";
 export default Ember.Component.extend({
 	limit: 5,
 	getData: function() {
-		if(this.get("model") == undefined) {
+		if(this.get("model") === undefined) {
 			this.set("model", this.get("store").query("article", {
 				_start: 0,
 				_limit: this.get("limit")
