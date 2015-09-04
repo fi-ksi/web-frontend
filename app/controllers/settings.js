575fcc23590b6c4a99a7df2990b348dc56e79be4
Jan Horacek <me@apophis.cz>
2015-09-04 13:04:53 +0200

    Add settings loading via ember helpers.

---
+++ app/controllers/settings.js
@@ -0,0 +1,7 @@
+import Ember from "ember";
+
+export default Ember.Controller.extend( {
+    countries: ["Česká republika", "Slovensko"],
+    tshirt_size: ["S", "M", "L", "XL"],
+    maturita_year: ["2016", "2017", "2018", "2019"]
+});
