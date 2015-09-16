212ea77d8e6214669c736e7a15bd3ad9101e55fa
Jan Mrázek <email@honzamrazek.cz>
2015-09-16 11:57:52 +0200

    Posting new thread works ok

--- app/controllers/forum.js
+++ app/controllers/forum.js
@@ -4,6 +4,10 @@ export default Ember.Controller.extend({
     actions: {
         new_thread: function() {
             this.set("new_thread", !this.get("new_thread"));
+            if(this.get("new_thread")) {
+            	this.set("thread_name", undefined);
+        		this.set("thread_content", undefined);
+            }
             this.set("thread_name_error", undefined);
             this.set("thread_content_error", undefined);
         },
@@ -22,6 +26,24 @@ export default Ember.Controller.extend({
         		return;
         	}
 
+            var thread = this.store.createRecord("thread", {
+                title: this.get("thread_name"),
+                root_posts: [
+                    this.store.createRecord("post", {
+                        author: this.get("session.profile.id"),
+                        body: this.get("thread_content")
+                    })
+                ]                
+            });
+
+            var post = this.store.createRecord("post", {
+                author: this.get("session.profile.id"),
+                body: this.get("thread_content")
+            });
+
+            thread.save().then(function() {
+                //post.save();
+            });
         	// Insert thread
         	/*this.store.push("thread", {
         		title: this.get("thread_name"),
@@ -35,6 +57,9 @@ export default Ember.Controller.extend({
 
         	this.set("new_thread", false);
         	this.set("info", "Vlákno úspěšně založeno");
+
+        	this.set("thread_name", undefined);
+        	this.set("thread_content", undefined);
 
         	var self = this;
         	Ember.run.later((function() {
