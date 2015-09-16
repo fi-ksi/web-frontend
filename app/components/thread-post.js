badf9360ccbefcbd4f8db933c20084a6bdda02ae
Jan Mrázek <email@honzamrazek.cz>
2015-09-16 14:39:54 +0200

    Implement new thread post sending

--- app/components/thread-post.js
+++ app/components/thread-post.js
@@ -4,7 +4,32 @@ export default Ember.Component.extend({
     actions: {
         react: function() {
             this.set("is_reacting", !this.get("is_reacting"));
+            this.set("content_error", undefined);
+            this.set("response_text", "");
+        },
+        send: function() {
+        	var self = this;
+
+        	if(!this.get("response_text")) {
+        		console.log("Error");
+        		this.set("content_error", "Nelze odeslat prázdný příspěvek");
+        		return;
+        	}
+        	this.set("content_error", undefined);
+        	var post = this.get("store").createRecord("post", {
+        		body: this.get("response_text")
+        	});
+
+        	post.save().then(function() {
+        		self.get("model.reaction").pushObject(post);
+        		self.get("model").save().then(function() {
+        			self.set("is_reacting", false);
+        		}, function() {
+        			self.set("content_error", "Nepodařilo se odeslat příspěvek");
+        		});
+        	});
         }
     },
-    is_reacting: false
+    is_reacting: false,
+    content_error: undefined
 });
--- app/controllers/forum.js
+++ app/controllers/forum.js
@@ -12,6 +12,7 @@ export default Ember.Controller.extend({
             this.set("thread_content_error", undefined);
         },
         post_thread: function() {
+            var self = this;
         	this.set("thread_name_error", undefined);
             this.set("thread_content_error", undefined);
 
@@ -42,29 +43,17 @@ export default Ember.Controller.extend({
             });
 
             thread.save().then(function() {
-                //post.save();
+                // ToDo: Error handling
+            	self.set("new_thread", false);
+            	self.set("info", "Vlákno úspěšně založeno");
+
+            	self.set("thread_name", undefined);
+            	self.set("thread_content", undefined);
+
+            	Ember.run.later((function() {
+    				self.set("info", undefined);
+    			}), 5000);
             });
-        	// Insert thread
-        	/*this.store.push("thread", {
-        		title: this.get("thread_name"),
-        		root_posts: [
-        			{
-        				author: this.get("session.profile.if"),
-        				body: this.get("thread_content"),
-        			}
-        		]
-        	});*/
-
-        	this.set("new_thread", false);
-        	this.set("info", "Vlákno úspěšně založeno");
-
-        	this.set("thread_name", undefined);
-        	this.set("thread_content", undefined);
-
-        	var self = this;
-        	Ember.run.later((function() {
-				self.set("info", undefined);
-			}), 5000);
         }
     },
     new_thread: false,
