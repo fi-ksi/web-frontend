66b6da936fbe28517bf6b6f2252d0813dadf4467
Jan Mrázek <email@honzamrazek.cz>
2015-09-18 10:14:59 +0200

    Fix graph loading

--- app/routes/tasks.js
+++ app/routes/tasks.js
@@ -3,6 +3,9 @@ import ResetScroll from '../mixins/reset-scroll';
 
 export default Ember.Route.extend(ResetScroll, {
 	model: function() {
-		return this.store.findAll("task", "");
+		return Ember.RSVP.hash({
+			tasks: this.store.findAll("task", ""),
+			categories: this.store.findAll("category", "") // Small hack - make sure all categories are loaded
+		});
 	},
 });
