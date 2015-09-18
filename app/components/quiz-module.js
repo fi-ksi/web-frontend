04d520243b372c810370a094d2559f0b9fccbea6
Jan Mrázek <email@honzamrazek.cz>
2015-09-19 01:11:12 +0200

    Quiz module implemented

--- app/components/quiz-module.js
+++ app/components/quiz-module.js
@@ -12,7 +12,33 @@ export default Ember.Component.extend({
     module_service: Ember.inject.service("module-service"),
     manage_submit: Ember.on("init", function() {
         this.get("module_service").on("submit", () => {
-            this.sendAction("error", "module_" + this.get("module").id);
+            var self = this;
+            var valid = true;
+            var response = [];
+            this.get("module.questions.questions").forEach(function(question, index) {
+                var checked = Ember.$(".group_" + self.get("id") + "_" + index).filter(function() {
+                    return Ember.$(this).is(":checked");
+                });
+                if(checked.length === 0) {
+                    // there aren't checked items!
+                    valid = false;
+                    console.log("Invalid! " + "#w_" + self.get("id") + "_" + index);
+                    response.push(undefined);
+                } else {
+                    Ember.$("#w_" + self.get("id") + "_" + index).addClass("hide");
+                    var c = [];
+                    checked.each(function() {
+                        c.push(Ember.$(this).attr("id").split("_").pop());
+                    });
+                    response.push(c);
+                }
+            });
+            if(!valid) {
+                this.sendAction("error", "module_" + this.get("module").id);
+                return;
+            }
+            
+            this.sendAction("result", "module_" + this.get("module").id, response);
         });
     })
 });
