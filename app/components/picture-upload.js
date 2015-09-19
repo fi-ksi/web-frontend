2a4f2016afa1419cb9da5469e8dc6c974bb54979
Jan Mrázek <email@honzamrazek.cz>
2015-09-19 12:04:30 +0200

    Implement better profile picture upload mechanism

--- app/components/picture-upload.js
+++ app/components/picture-upload.js
@@ -5,40 +5,37 @@ import InboundActions from "ember-component-inbound-actions/inbound-actions";
 export default EmberUploader.FileField.extend(InboundActions, {
 	url: "",
 	supported_ext: ["jpg", "jpeg", "png", "gif"],
-	files: undefined,
-	actions: {
-		make_upload: function() {
-			var uploader = EmberUploader.Uploader.create({
-				url: this.get("url")
-			});
-
-			uploader.on("didUpload", function() {
-				console.log("Upload finished");
-				this.sendAction("upload_finished");
-			});
-
-			uploader.on("didError", function(jqXHR, textStatus, errorThrown) {
-				console.log("Upload canceled");
-				this.sendAction("upload_failed", textStatus, errorThrown);
-			});
-
-			if (!Ember.isEmpty(this.get("files"))) {
-				uploader.upload(this.get("files")[0]);
-			}
-		}
-	},
+	classNames: ["hide"],
+	attributeBindings: ["multiple", "accept"],
+	accept: function() {
+		return this.get("supported_ext").map(function(i) { return "." + i; }).toString();
+	}.property("supported_ext"),
 	filesDidChange: function(files) {
-		this.set("files", undefined);
 		for(var i = 0; i !== files.length; i++) {
 			var file = files.item(i).name;
 			console.log(file);
 			console.log(file.split(".").pop().toLowerCase());
 			if (this.get("supported_ext").indexOf(file.split(".").pop().toLowerCase()) === -1) {
-				this.sendAction("file_type_error", file);
-				console.log("Invalid file");
+				alert("Nepodporovaný typ souboru: " + file);
 				return;
 			}
 		}
-		this.set("files", files);
+		var uploader = EmberUploader.Uploader.create({
+			url: this.get("url")
+		});
+
+		uploader.on("didUpload", function() {
+			console.log("Upload finished");
+			this.sendAction("upload_finished");
+		});
+
+		uploader.on("didError", function(jqXHR, textStatus, errorThrown) {
+			console.log("Upload canceled");
+			this.sendAction("upload_failed", textStatus, errorThrown);
+		});
+
+		if (!Ember.isEmpty(this.get("files"))) {
+			uploader.upload(this.get("files")[0]);
+		}
 	}
 });
--- app/controllers/settings.js
+++ app/controllers/settings.js
@@ -4,34 +4,18 @@ export default Ember.Controller.extend( {
     countries: ["Česká republika", "Slovensko"],
     tshirt_size: ["S", "M", "L", "XL"],
     maturita_year: ["2016", "2017", "2018", "2019"],
-    is_changing_picture: false,
-    picture_error: undefined,
-    picture_info: undefined,
     global_error: undefined,
     global_info: undefined,
     actions: {
     	upload_pic: function() {
-    		if(this.get("is_changing_picture")) {
-    			this.get("picture_field").send("make_upload");
-    			this.set("picture_info", "Obrázek se nahrává");
-    		}
-    		else {
-    			this.set("is_changing_picture", true);
-    			this.set("picture_error", undefined);
-    			this.set("picture_info", undefined);
-    		}
-    	},
-    	file_type_error: function(filename) {
-    		console.log("Invalid file captured");
-    		this.set("picture_error", "Nepodporovaný typ souboru: " + filename);
+    		Ember.$("#picture_input").trigger('click');
     	},
     	upload_finished: function() {
     		// ToDo: Reload picture!
-    		this.set("is_changing_picture", false);
+            alert("Obrázek změnen!");
     	},
     	upload_failed: function(status, error) {
-    		this.set("picture_info", undefined);
-    		this.set("picture_error", "Nepodařilo se nahrát obrázek: " + status + " " + error);
+    		alert("Nepodařilo se nahrát obrázek: " + status + " " + error);
     	},
     	save: function() {
     		var self = this;
