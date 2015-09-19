5c83dcd66c98411c5157b4dbc78729a698c7bcc3
Jan Mrázek <email@honzamrazek.cz>
2015-09-19 23:28:54 +0200

    Implement task deadline

--- app/controllers/task/assignment.js
+++ app/controllers/task/assignment.js
@@ -2,6 +2,23 @@ import Ember from "ember";
 
 export default Ember.Controller.extend({
 	module_service: Ember.inject.service('module-service'),
+	resubmit: false,
+	opened: Ember.computed("model.time_deadline", function() {
+		if(!this.get("model.time_deadline")) {
+			return true;
+		}
+		var currentdate = new Date();
+		if (this.get("model.time_deadline") > currentdate) {
+			return true;
+		}
+		return false;
+	}),
+	show_submission: Ember.computed("resubmit", "model.submissions", function() {
+		if(this.get("resubmit")) {
+			return true;
+		}
+		return Ember.isEmpty(this.get("model.submissions"));
+	}),
 	actions: {
 		submit: function() {
 			this.get("module_service").emit_submit();
@@ -12,6 +29,9 @@ export default Ember.Controller.extend({
 		},
 		error: function(id) {
 			console.log(id + " reports error");
+		},
+		new_submit: function() {
+			this.set("resubmit", true);
 		}
 	}
 });
