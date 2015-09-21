cedf6266957edaa31e07e41f38dc667f3a020400
Jan Mrázek <email@honzamrazek.cz>
2015-09-21 13:07:15 +0200

    Add missing templates

---
+++ app/routes/registration.js
@@ -0,0 +1,9 @@
+import Ember from "ember";
+import ResetScroll from '../mixins/reset-scroll';
+
+export default Ember.Route.extend(ResetScroll, {
+	model: function() {
+		var profile = this.store.createRecord('profile');
+		return profile;
+	},
+});
