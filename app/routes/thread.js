8cdbaedebf1dcdf2bcdcc2c1b146ff02f17cee23
Jan Mrázek <email@honzamrazek.cz>
2015-09-29 11:53:11 +0200

    Add PUT request after visiting thread

--- app/routes/thread.js
+++ app/routes/thread.js
@@ -1,14 +1,26 @@
 import Ember from "ember";
 import ResetScroll from '../mixins/reset-scroll';
+import config from '../config/environment';
 
 export default Ember.Route.extend(ResetScroll, {
 	model: function(params) {
+		this.set("thread_id", params["thread_id"]);
 		return this.store.find("thread", params["thread_id"]);
+	},
+	afterModel: function() {
+		// Non-system hack
+		Ember.$.ajax({
+            url: config.API_LOC + "/threads/" + this.get("thread_id"),
+            data: {},
+            contentType: "application/json",
+            type: 'PUT',
+        });
 	},
 	titleToken: function(model) {
 		return model.get("title");
 	},
 	title: function(tokens) {
 		return "KSI: " + tokens.pop();
-	}
+	},
+	thread_id: undefined
 });
