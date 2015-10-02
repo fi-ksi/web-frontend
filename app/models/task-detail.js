3a6493b568b2e4b30d22d243755adfb4c9697a6d
Jan Mrázek <email@honzamrazek.cz>
2015-10-02 22:04:41 +0200

    Fi statistics

--- app/models/task-detail.js
+++ app/models/task-detail.js
@@ -7,7 +7,7 @@ export default DS.Model.extend( {
 	thread: DS.belongsTo("thread",  { async: true }),
 	modules: DS.hasMany("module"),
 
-	best_scores: DS.hasMany("user-score"), // Top 5
+	best_scores: DS.hasMany("user-score", { async: true }), // Top 5
 
 	comment: DS.belongsTo("thread", { async: true }),
 	achievements: DS.hasMany("achievements", { async: true })
