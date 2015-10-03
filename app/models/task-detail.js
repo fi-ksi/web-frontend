fee19fd577b5b43fb94a2b9729981441c2e0bd8d
Jan Mrázek <email@honzamrazek.cz>
2015-10-03 21:52:48 +0200

    Make comment and task thread included

--- app/models/task-detail.js
+++ app/models/task-detail.js
@@ -3,11 +3,11 @@ import DS from "ember-data";
 export default DS.Model.extend( {
 	body: DS.attr("string"),
 	solution: DS.attr("string"),
-	thread: DS.belongsTo("thread",  { async: true }),
+	thread: DS.belongsTo("thread"),
 	modules: DS.hasMany("module"),
 
 	best_scores: DS.hasMany("user-score"), // Top 5
 
-	comment: DS.belongsTo("thread", { async: true }),
+	comment: DS.belongsTo("thread"),
 	achievements: DS.hasMany("achievements", { async: true })
 });
