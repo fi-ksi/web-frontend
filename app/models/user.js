089550bbd411b733badd8bc37d14a283fe5216bf
Jan Mrázek <email@honzamrazek.cz>
2015-10-08 23:53:52 +0200

    Fix nickname

--- app/models/user.js
+++ app/models/user.js
@@ -8,7 +8,7 @@ export default DS.Model.extend( {
     nick_name: DS.attr("string"),
     full_name: Ember.computed("first_name", "nick_name", "last_name", function() {
         //console.log(this.get("nick_name"));
-        if(this.get("nick_name") === undefined || this.get("nick_name").length === 0) {
+        if(!this.get("nick_name") || this.get("nick_name").length === 0) {
             return this.get("first_name") + ' ' + this.get("last_name");
         }
         return this.get("first_name") + ' "' + this.get("nick_name") + '" ' + this.get("last_name");
