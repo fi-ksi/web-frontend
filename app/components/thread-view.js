6ac1f0d2ec38791d243a4aaf7a880d3ad3a2a69c
Jan Mrázek <email@honzamrazek.cz>
2015-10-10 18:49:31 +0200

    Allow adding root posts (close #76)

--- app/components/thread-view.js
+++ app/components/thread-view.js
@@ -14,5 +14,29 @@ export default Ember.Component.extend({
             contentType: "application/json",
             type: 'PUT',
         });
-    }.observes("thread")
+    }.observes("thread"),
+    actions: {
+        add_comment: function() {
+            this.set("is_reacting", !this.get("is_reacting"));
+            this.set("content_error", undefined);
+            this.set("response_text", "");
+        },
+        send: function() {
+            var self = this;
+
+            if(!this.get("response_text")) {
+                this.set("content_error", "Nelze odeslat prázdný příspěvek");
+                return;
+            }
+            this.set("content_error", undefined);
+            var post = this.get("store").createRecord("post", {
+                body: self.get("response_text"),
+                thread: self.get("thread")
+            });
+
+            post.save().then(function() {
+                self.get("thread.details.root_posts").pushObject(post);
+            });
+        }
+    },
 });
