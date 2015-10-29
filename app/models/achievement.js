76ea35c8c26fa21b188d3d90ae53e8b0ec0d15c5
Jan Horacek <me@apophis.cz>
2015-10-30 00:50:44 +0100

    Add description to achievement.

--- app/models/achievement.js
+++ app/models/achievement.js
@@ -1,7 +1,8 @@
 import DS from "ember-data";
 
 export default DS.Model.extend( {
-	title: DS.attr("string"),
-	picture: DS.attr("string"),
- 	body: DS.attr("string")
+    title: DS.attr("string"),
+    picture: DS.attr("string"),
+    body: DS.attr("string"),
+    description: DS.attr("string")
 });
