cebe30106e4f9be41a5c36430f335f91a7fcac1b
Jan Mrázek <email@honzamrazek.cz>
2015-11-22 23:58:01 +0100

    Meh

--- app/controllers/task.js
+++ app/controllers/task.js
@@ -2,7 +2,6 @@ import Ember from "ember";
 
 export default Ember.Controller.extend({
 	mathObserver: function() {
-		alert("Here");
         MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
     }.observes("model.details.body")
 });
