57dd30af6e5e48abc4328f523c6a52c3c56d2f0c
Jan Mrázek <email@honzamrazek.cz>
2015-12-09 22:39:17 +0100

    Change the way corrected/not corrected is displayed

--- app/models/module-score.js
+++ app/models/module-score.js
@@ -3,4 +3,5 @@ import DS from "ember-data";
 export default DS.Model.extend( {
 	reviewed_by: DS.belongsTo("user", {async: true, default: null}),
 	score: DS.attr("number"),
+	is_corrected: DS.attr("boolean")
 });
