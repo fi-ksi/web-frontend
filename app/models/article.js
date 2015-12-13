9be8f9d5bfb87436f2211d7ce0580a2cecb97968
Jan Horacek <me@apophis.cz>
2015-12-13 14:19:38 +0100

    Add admin articles list.

--- app/models/article.js
+++ app/models/article.js
@@ -6,6 +6,14 @@ export default DS.Model.extend( {
 	time_published: DS.attr("date"),
 	picture: DS.attr("string"),
 	body: DS.attr("string"),
+	published: DS.attr("boolean"),
+
+	published_str: Ember.computed("published", function() {
+		if (this.get("published"))
+			return "ano";
+		else
+			return "ne";
+	}),
 	intro: Ember.computed("body", function() {
 		var begin = this.get("body").indexOf("<p>");
 		var end = this.get("body").indexOf("</p>");
