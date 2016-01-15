de04f6434bd2604bc66ae2d2ef25f48f6a214422
Jan Horacek <me@apophis.cz>
2016-01-15 20:06:59 +0100

    Enhance article editing.

--- app/controllers/admin/articles.js
+++ app/controllers/admin/articles.js
@@ -1,5 +1,17 @@
 import Ember from "ember";
 
 export default Ember.Controller.extend( {
+    store: Ember.inject.service(),
+    session: Ember.inject.service(),
 
+    actions: {
+        'article-delete': function(article) {
+            if(!confirm("Opravdu odstranit článek " + article.get("title") + "?")) {
+                return;
+            }
+
+             article.set("deleting", true);
+             article.destroyRecord(); // DELETE to /admin/atask/1
+        },
+    },
 });
