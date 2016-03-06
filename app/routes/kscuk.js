7d910865fbf08bdb91c72fcd9817ce75aecf7a48
Jan Horacek <me@apophis.cz>
2016-03-06 02:12:19 +0100

    Fix warning.

--- app/routes/kscuk.js
+++ app/routes/kscuk.js
@@ -1,7 +1,7 @@
 import Ember from "ember";
 
 export default Ember.Route.extend({
-    beforeModel: function(param) {
+    beforeModel: function() {
         window.location.replace("http://kscuk.fi.muni.cz/");
     },
     view: null,
