8169192a63538e38d46fd60089d6e07b3166b8c0
Jan Horacek <me@apophis.cz>
2016-03-14 10:12:44 +0100

    Fix maturita year.

--- app/mixins/user-settings.js
+++ app/mixins/user-settings.js
@@ -1,7 +1,7 @@
 import Ember from "ember";
 
 export default Ember.Mixin.create({
-	countries: [{name: "Česká republika", short: "cz"}, {name: "Slovensko", short: "sk"}],
+    countries: [{name: "Česká republika", short: "cz"}, {name: "Slovensko", short: "sk"}],
     tshirt_size: ["S", "M", "L", "XL"],
-    maturita_year: ["2016", "2017", "2018", "2019", "2020", "2021", "2022", "2023"], // ToDo: Implement variable
+    maturita_year: [2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023], // ToDo: Implement variable
 });
