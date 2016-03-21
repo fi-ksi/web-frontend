31d340d183ea605be5b8bf93eb8aa6cf1ff8aecb
Jan Horacek <me@apophis.cz>
2016-03-21 13:22:52 +0100

    Fix warning.

--- app/routes/admin/achievement-create.js
+++ app/routes/admin/achievement-create.js
@@ -2,7 +2,7 @@ import Ember from "ember";
 import ResetScroll from '../../mixins/reset-scroll';
 
 export default Ember.Route.extend(ResetScroll, {
-    model: function(params) {
+    model: function() {
         return this.store.find("content", "achievements");
     },
     title: "KSI: Nová trofej",
