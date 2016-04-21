335bddb4117d02581084e8cecf75b0661f0aff37
Jan Horacek <me@apophis.cz>
2016-04-21 21:05:42 +0200

    Sort articles in administration.

--- app/controllers/admin/articles.js
+++ app/controllers/admin/articles.js
@@ -3,6 +3,9 @@ import Ember from "ember";
 export default Ember.Controller.extend( {
     store: Ember.inject.service(),
     session: Ember.inject.service(),
+    sortProperties:  [ "time_published:desc" ],
+
+    articles: Ember.computed.sort("model", "sortProperties"),
 
     actions: {
         'article-delete': function(article) {
