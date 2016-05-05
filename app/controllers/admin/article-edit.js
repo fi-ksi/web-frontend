8081cd1e481027c0cfa5404b6a5cc8c4fa36e678
Jan Horacek <me@apophis.cz>
2016-05-05 12:36:25 +0200

    Show "saving" when saving article.
    Close #247.

--- app/controllers/admin/article-edit.js
+++ app/controllers/admin/article-edit.js
@@ -4,13 +4,21 @@ export default Ember.Controller.extend( {
     session: Ember.inject.service(),
     save_status: "",
     error_status: "",
+    saving: false,
 
     actions: {
         'article-save': function() {
             var self = this;
+            this.set("saving", true);
+            this.set("save_status", "");
+            this.set("error_status", "");
             this.get("model").save().then(function(){
-                self.set("save_status", "Článek uložen");
+                var date = new Date();
+                self.set("saving", false);
+                self.set("save_status", "Článek uložen.");
+                setTimeout(function(){ self.set("save_status", ""); }, 5000);
             }, function(){
+                self.set("saving", false);
                 self.set("error_status", "Špatná odpověď ze serveru! Zkus to za chvíli znovu. Pokud problém přetrvává, kontaktuj organizátora.");
             });
         }
