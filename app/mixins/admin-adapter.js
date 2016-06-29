f2d4c45d3927d486011dc88d48e30588c21efcc2
Jan Horacek <me@apophis.cz>
2016-06-30 00:36:02 +0200

    Add year select.

--- app/mixins/admin-adapter.js
+++ app/mixins/admin-adapter.js
@@ -3,7 +3,17 @@ import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 import config from '../config/environment';
 
 export default DS.RESTAdapter.extend(DataAdapterMixin, {
-	authorizer: 'authorizer:oauth2',
-	namespace: 'admin',
-    host: config.API_LOC
+    authorizer: 'authorizer:oauth2',
+    namespace: 'admin',
+    host: config.API_LOC,
+
+    headers: Ember.computed('session.year', function() {
+        var year = this.get('session.year');
+        if (year) {
+            return { 'year': year }
+        } else {
+            return {}
+        }
+    })
+
 });
--- app/services/session.js
+++ app/services/session.js
@@ -1,12 +1,63 @@
 import Ember from 'ember';
 import SessionService from 'ember-simple-auth/services/session';
+import config from '../config/environment';
 
 export default SessionService.extend({
     store: Ember.inject.service(),
     setCurrentUser: function() {
-	var self = this;
-	return this.get("store").find("profile", "").then(function(user) {
-	        self.set("current_user", user);
-	    });
-	}.observes("isAuthenticated")
+        var self = this;
+        return this.get("store").find("profile", "").then(function(user) {
+            self.set("current_user", user);
+        });
+    }.observes("isAuthenticated"),
+
+    year: null,
+    years: [],
+
+    init() {
+        this._super();
+
+        // get last year
+        /* this request must be done as AJAX
+         * this.get('session').findAll('year')...
+         * does not work (it leads to infinite recursion in the session service)
+         */
+        var self = this;
+        Ember.$.ajax({
+            url: config.API_LOC + "/years/",
+            type: 'GET',
+            success: function(data) {
+                self.set('years', data['years']);
+                self.set('year', data['years'].get('lastObject').id);
+            },
+            error: function() {
+                self.set('years', []);
+                self.set('year', 10);
+            }
+        });
+    },
+
+    unloadSensitive: function() {
+        var store = this.get('store');
+        store.unloadAll('achievement');
+        store.unloadAll('article');
+        store.unloadAll('atask');
+        store.unloadAll('correction');
+        store.unloadAll('corrections-info');
+        store.unloadAll('task');
+        store.unloadAll('task-detail');
+        store.unloadAll('task-score');
+        store.unloadAll('thread');
+        store.unloadAll('thread-detail');
+        store.unloadAll('post');
+        store.unloadAll('module');
+        store.unloadAll('module-score');
+        store.unloadAll('user');
+        store.unloadAll('wave');
+    },
+
+    changeYear: function(newyear) {
+        this.set('year', newyear);
+        this.unloadSensitive();
+   }
 });
