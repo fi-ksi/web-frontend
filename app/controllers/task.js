0d9026daff938131f12f7816c759e8ecd983baf2
Jan Horacek <me@apophis.cz>
2016-08-04 20:44:07 +0300

    Numbers in czech language.

--- app/controllers/task.js
+++ app/controllers/task.js
@@ -3,5 +3,12 @@ import Ember from "ember";
 export default Ember.Controller.extend({
     mathObserver: function() {
         MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
-    }.observes("model.details.body")
+    }.observes("model.details.body"),
+
+    points_text: Ember.computed("model.max_score", function(){
+        var points = this.get("model.max_score");
+        if (points === 1) { return "bod"; }
+        else if ((points === 2) || (points === 3) || (points === 4)) { return "body"; }
+        else { return "bodů"; }
+    }),
 });
