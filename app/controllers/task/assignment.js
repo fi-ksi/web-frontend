e59a81f37a07ed0906be122d4f5274fc0fd0f55a
Jan Horacek <me@apophis.cz>
2016-09-20 22:48:40 +0200

    Remove deprecated code.

--- app/controllers/task/assignment.js
+++ app/controllers/task/assignment.js
@@ -33,55 +33,6 @@ export default Ember.Controller.extend({
         return this.get("model.modules").map(function(module) { return "module_" + module.get("id"); });
     }),
     submit_error: "",
-    submit_all: function() {
-        var self = this;
-        this.set("submit_error", "");
-        if(this.get("module_ids").every(function(id) { return id in self.get("results"); } )) {
-            // Check if all components have suceeed
-            if(this.get("module_ids").every(function(id) {return self.get("results." + id + ".type") === "success"; })) {
-                // Send all items!
-                var data = new FormData();
-                var results = this.get("results");
-
-                for (var property in results) {
-                    if (results.hasOwnProperty(property)) {
-                        var result = results[property]["data"];
-                        if("files" in result) {
-                            // This is file module
-                            for (var i = 0; i !== result["files"].length; i++) {
-                                data.append(property + "_file_" + i, result["files"][i]);
-                            }
-                        } else {
-                            data.append(property, JSON.stringify(result));
-                        }
-                    }
-                }
-
-                // Send it
-                Ember.$.ajax({
-                    url: config.API_LOC + "/task/" + this.get("model.id") + "/submit",
-                    data: data,
-                    processData: false,
-                    contentType: false,
-                    type: 'POST',
-                    success: function(data){
-                        alert(data);
-                        this.get("model").reload();
-                    },
-                    error: function(xhr, textStatus, errorThrown) {
-                        self.set("submit_error", errorThrown);
-                    }
-                });
-            } else {
-                // Scroll to highest error
-                var elems = Ember.$(".alert-danger");
-                var offset = Math.min.apply(null, Ember.$.map(elems, function(elem) { return Ember.$(elem).offset().top; }));
-                Ember.$('html, body').animate({
-                    scrollTop: offset - 150 //Magic constant
-                }, "slow");
-            }
-        }
-    },
     actions: {
         submit: function() {
             var self = this;
