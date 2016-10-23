7827867828909ce445d9b5c175643423e191a74f
Jan Horacek <me@apophis.cz>
2016-10-23 21:07:31 +0200

    Add old orgs.

---
+++ app/components/orgs-const.js
@@ -0,0 +1,9 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+    orgs: [
+        {name:"Ondra Bouda", profile:"ondra-bouda.jpg"},
+        {name:"Radek Pelánek", profile:"radek-pelanek.jpg"},
+        {name:"Pavel Šimeček", profile:"pavel-simecek.jpg"},
+    ]
+});
