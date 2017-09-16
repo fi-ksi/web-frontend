c960ad69588fc15d274415be9edf12562c3a4d49
Jan Horacek <me@apophis.cz>
2017-09-16 18:35:41 +0200

    Achievements are part of user`s data.

--- app/models/user-score.js
+++ app/models/user-score.js
@@ -3,5 +3,4 @@ import DS from "ember-data";
 export default DS.Model.extend( {
     user: DS.belongsTo("user", {async: true}),
     score: DS.attr("number"),
-    achievements: DS.hasMany("achievement", {async: true})
 });
