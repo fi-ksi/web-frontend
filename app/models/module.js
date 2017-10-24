4689c66910aaaa01080a05fa12807b5c98fad1ea
Ondřej Borýsek <ondrej.borysek+github@gmail.com>
2017-10-24 16:09:39 +0200

    Because doing merge correctly on first try is impossible

--- app/models/module.js
+++ app/models/module.js
@@ -44,7 +44,7 @@ export default DS.Model.extend( {
     // For code
     code: DS.attr("string"),
     default_code: DS.attr("string"),
-    last_datetime: DS.attr("string"),
+    last_datetime: DS.attr("date"),
     last_origin: DS.attr("string"),
 
     last_origin_text: Ember.computed("last_origin", function() {
