dd605fe3755a2e82d2151b5967904cdc0b8ee39f
Ondřej Borýsek <BorysekOndrej@users.noreply.github.com>
2018-08-27 15:46:42 +0200

    Working communication

--- app/components/task-feedback.js
+++ app/components/task-feedback.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import config from '../config/environment';
 
 export default Ember.Component.extend({
     session: Ember.inject.service(),
@@ -7,14 +8,48 @@ export default Ember.Component.extend({
     save: function() {
         var self = this;
         this.set("statemsg", "Ukládám");
-        this.get("model").save().then(
-            function() {
-                self.set("statemsg", "Uloženo");
-            },
-            function() {
-                self.set("statemsg", "Chyba! při ukládání. Zkus znovu");
+        var data_obj  = JSON.stringify(this.get("model.feedbacks"), null, 2);
+        var data_json = JSON.parse(data_obj);
+        var categories = data_json["content"]["categories"];
+        for (var i = 0; i < categories.length; i++) {
+            const element_name = data_json["content"]["categories"][i]["id"];
+            let answer = Ember.$("#task-feedback-answer-"+element_name).val();
+            let ftype = data_json["content"]["categories"][i]["ftype"];
+            if (ftype === "stars" || ftype === "line"){
+                if (answer === ""){
+                    answer = 0;
+                }else{
+                    answer = parseInt(answer);
+                }
             }
-        );
+            data_json["content"]["categories"][i]["answer"] = answer;
+        }
+        data_json["content"]["filled"] = true;
+        data_json["content"]["id"] = self.get("model").id;
+
+        data_json["feedback"] = data_json["content"];
+        delete data_json["isFulfilled"];
+        delete data_json["content"];
+        var data_json_string  = JSON.stringify(data_json, null, 2);
+        this.get('session').authorize('authorizer:oauth2', function(header, h) {
+            Ember.$.ajax({
+                url: config.API_LOC + "/feedbacks/"+self.get("model").id,
+                data: data_json_string,
+                contentType: "application/json",
+                type: 'PUT',
+                beforeSend: function(xhr) {
+                    xhr.setRequestHeader(header, h);
+                },
+                success: function() {
+                },
+                error: function(resp) {
+                    self.set("feedback_sending", false);
+                    var e = "Už ani nahlásit chybu nejde, za to beztak může Los Karlos! Napiš nám na ksi@fi.muni.cz.<br>" + resp.message;
+                    if ((resp.errors) && (resp.errors[0])) { e += "<br>" + resp.errors[0].status  + " : " + resp.errors[0].title; }
+                    self.set("feedback_error", e);
+                }
+            });
+        });
     },
 
     statemsg: "Uloženo",
--- app/models/feedback.js
+++ app/models/feedback.js
@@ -3,7 +3,7 @@ import DS from "ember-data";
 export default DS.Model.extend( {
     userId: DS.attr("number"),
     // id: DS.belongsTo("task"), // cannot be assigned
-    filled: DS.attr("string"),
-    categories: DS.hasMany("feedback-category"),
+    filled: DS.attr("boolean"),
+    categories: DS.attr(),
     //categories: DS.attr(),
 });
