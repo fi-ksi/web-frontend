1abc9b14fa0439315d02793bac45f6e6b03268b7
Ondřej Borýsek <BorysekOndrej@users.noreply.github.com>
2019-09-02 03:15:35 +0200

    slider possibly final fix

--- app/helpers/feedback-slider-default.js
+++ app/helpers/feedback-slider-default.js
@@ -2,7 +2,7 @@ import Ember from "ember";
 
 export default Ember.Helper.helper(function(val) {
     debugger;
-    if (val === "" || val === undefined){
+    if (val === "" || val === undefined || val[0] == undefined){
         return 3;
     }
     return val;
