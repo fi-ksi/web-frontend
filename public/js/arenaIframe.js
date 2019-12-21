6b1bf9355fcd12e2b966a6ed03fae51b9cecd172
Ondřej Borýsek <BorysekOndrej@users.noreply.github.com>
2019-12-21 15:37:32 +0100

    arena iframe basic tests

--- public/js/arenaIframe.js
+++ public/js/arenaIframe.js
@@ -1,2 +1,15 @@
-console.log("Arena-test1");
-debugger;
+console.log("Arena-loadJS");
+
+
+$( document ).ready(function() {
+    console.log( "Arena-documentReady");
+    setInterval(placeArenaIframeInDiv, 5*1000);
+    // debugger;
+});
+
+
+function placeArenaIframeInDiv(){
+    console.log(window.location);
+}
+
+
