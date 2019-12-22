fe216f28f1d20df4e8c5bcd9f87e6b0a16e7a7ac
Ondřej Borýsek <BorysekOndrej@users.noreply.github.com>
2019-12-22 23:25:23 +0100

    update to include arena stats

--- public/js/arenaIframe.js
+++ public/js/arenaIframe.js
@@ -2,7 +2,8 @@ arenaDebugLog("Arena-loadJS");
 
 $( document ).ready(function() {
     arenaDebugLog( "Arena-documentReady");
-    setInterval(placeArenaIframeInDiv, 5*1000);
+    setInterval(place_single_user, 8*1000);
+    setInterval(place_stats, 9*1000);
     // debugger;
 });
 
@@ -14,21 +15,34 @@ function arenaDebugLog(msg){
     console.log(msg);
 }
 
-function placeArenaIframeInDiv(){
+function place_single_user(){
+    var divSelector = "#arena-iframe-to-replace";
+    var iframeId = "arena-iframe-actual-iframe"; // ! not the selector
+    var url = 'https://ksi-api-html.borysek.eu/logPage.html?userId=';
+    placeArenaIframeInDiv(divSelector, iframeId, url);
+}
+
+function place_stats(){
+    var divSelector = "#arena-statistics-iframe-to-replace";
+    var iframeId = "arena-iframe-actual-iframe2"; // ! not the selector
+    var url = 'https://ksi-api-html.borysek.eu/arenaStats.html?userId=';
+    placeArenaIframeInDiv(divSelector, iframeId, url);
+}
+
+function placeArenaIframeInDiv(divSelector, iframeId, url){
     var curPath = window.location.pathname;
+    arenaDebugLog("Arena: " + divSelector);
     arenaDebugLog(curPath);
     if (curPath != "/ulohy/305"){
         return;
     }
     arenaDebugLog("Arena-rightPage");
-    var divSelector = "#arena-iframe-to-replace";
-    var iframeId = "arena-iframe-actual-iframe"; // ! not the selector
     var usedIDSelector = "#id-of-logged-in-user";
 
     if ($(divSelector).find("iframe")[0] === undefined){
         arenaDebugLog("Arena-iframeMissing");
         var userID = $(usedIDSelector).text().trim();
-        var textPlacedWithin = '<iframe id="'+iframeId+'" src="https://ksi-api-html.borysek.eu/logPage.html?userId='+userID+'" style="width:100%; border:none; height: 600px;"></iframe>';
+        var textPlacedWithin = '<iframe id="'+iframeId+'" src="'+url+userID+'" style="width:100%; border:none; height: 600px;"></iframe>';
         $(divSelector).first().html(textPlacedWithin);
         /*
         $("#"+iframeSelector).on("load", function() {
