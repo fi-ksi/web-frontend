4b9bf82f1e1a4ec0ff5787e923101fac1e6fdc28
Ondřej Borýsek <BorysekOndrej@users.noreply.github.com>
2022-01-21 09:34:12 +0100

    Improve tracking code to track movement across the site

--- public/js/matomo.js
+++ public/js/matomo.js
@@ -12,3 +12,33 @@
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
     g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
   })();
+
+//window.onpopstate = function(event) { // this didn't work reliably
+function trackNewPage(){
+    _paq.push(['setReferrerUrl', location.href]);
+    _paq.push(['setCustomUrl', window.location.pathname]);
+    _paq.push(['setDocumentTitle', document.title]);
+
+    // remove all previously assigned custom variables, requires Matomo (formerly Piwik) 3.0.2
+    _paq.push(['deleteCustomVariables', 'page']); 
+    _paq.push(['trackPageView']);
+
+    // make Matomo aware of newly added content
+    var content = document.getElementById('content');
+    // _paq.push(['MediaAnalytics::scanForMedia', content]);
+    // _paq.push(['FormAnalytics::scanForForms', content]);
+    _paq.push(['trackContentImpressionsWithinNode', content]);
+    _paq.push(['enableLinkTracking']);
+};
+
+// modified https://stackoverflow.com/a/4585031
+// this works, but document.title is not yet updated at this point
+
+// modified https://stackoverflow.com/a/29540461
+new MutationObserver(function(mutations) {
+    // console.log(document.title);
+    trackNewPage();
+}).observe(
+    document.querySelector('title'),
+    { subtree: true, characterData: true, childList: true }
+);
